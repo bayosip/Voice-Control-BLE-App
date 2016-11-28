1c8d829058116bdc32dac68f5bc324f69569de08
Adebayo <adebayojnr91@gmail.com>
2016-11-27 20:28:07 -0500

    Created UI Fragment XML file and classes for the Control Activity

--- app/src/main/java/inc/osips/bleproject/Controller/VoiceControlFragment.java
+++ app/src/main/java/inc/osips/bleproject/Controller/VoiceControlFragment.java
@@ -1,23 +1,29 @@
 package inc.osips.bleproject.Controller;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import java.util.zip.Inflater;
+import java.util.ArrayList;
+import inc.osips.bleproject.Model.VoiceRecognition;
 import inc.osips.bleproject.R;
-import android.app.Activity;
+import inc.osips.bleproject.Utilities.ToastMessages;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import static android.app.Activity.RESULT_OK;
 
 public class VoiceControlFragment extends Fragment {
 
     private ImageButton buttonSpeak;
     private TextView interpretedText;
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.voice_control, container, false);
         initialiseWidgets(view);
         return view;
@@ -29,8 +35,26 @@ public class VoiceControlFragment extends Fragment {
         buttonSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                interpretedText.setText("You said: ");
+                try {
+                    startActivityForResult(VoiceRecognition.speechInputCall(), 100);
+
+                }catch (ActivityNotFoundException e){
+                    ToastMessages.message(getContext(),e.getMessage());
+                }
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch (requestCode){
+            case 100:
+                if ((resultCode == RESULT_OK)&& (intent!=null)){
+                    ArrayList<String> instructions = intent.getStringArrayListExtra(
+                            RecognizerIntent.EXTRA_RESULTS);
+                    interpretedText.setText("You said: " + instructions.get(0));
+                }
+                break;
+        }super.onActivityResult(requestCode, resultCode, intent);
     }
 }
